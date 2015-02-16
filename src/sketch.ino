f8f8472daca1ddc64e3056fba6131b77ef075d65
Philipp Klaus <philipp.klaus@gmail.com>
2015-02-16 23:18:30 +0100

    MAX31850: Fixing broken logic around NaN

--- src/sketch.ino
+++ src/sketch.ino
@@ -153,11 +153,11 @@ bool handle_max31850() {
     while (max31850_tries < max31850_max_tries) {
       max31850_tries++;
       tempC = sensors.getTempC(max31850_temp_device_address);
-      if (tempC < -270.0 || tempC > 1768.0 || tempC == NAN) continue;
+      if (tempC < -270.0 || tempC > 1768.0 || tempC != tempC) continue;
       else break;
     }
     long int tempCint;
-    if (tempC >= -270.0 || tempC <= 1768.0 || tempC != NAN) {
+    if (tempC >= -270.0 && tempC <= 1768.0 && tempC == tempC) {
       tempCint = (long int) (tempC * 100.0);
     } else {
       // we failed to read this sensor ->  let's send an error message
